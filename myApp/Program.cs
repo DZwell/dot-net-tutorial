332d62bfd3faf93502abe5c945b8001a439ff653
Daniel Zwelling <dzwellingmusic@gmail.com>
2018-05-09 15:33:28 -0700

    Initial commit

---
+++ myApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace list_quickstart
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      WorkingWithStrings();
+    }
+
+    public static void WorkingWithStrings()
+    {
+      var names = new List<string> { "dzwell", "Ana", "Felipe" };
+
+      names.Add("Maria");
+      names.Add("Justo");
+      names.Remove("Ana");
+
+      foreach (var name in names)
+      {
+        Console.WriteLine($"Hello {name.ToLower()}!");
+      }
+      Console.WriteLine($"Name at index 0 is {names[0]}");
+      Console.WriteLine($"There are {names.Count} people in the list");
+    }
+  }
+}
