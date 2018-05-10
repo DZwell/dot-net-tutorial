50a369c7cb3e2924c748d558ca9ff1979b9ed372
Daniel Zwelling <dzwellingmusic@gmail.com>
2018-05-10 12:19:47 -0700

    Start classes

---
+++ classes/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace classes {
+  class Program {
+    static void Main (string[] args) {
+      var account = new BankAccount ("Me", 1000);
+      Console.WriteLine ($"Account owner: {account.Owner}\nAccount number: {account.Number}\nBalance: {account.Balance}");
+    }
+  }
+}
